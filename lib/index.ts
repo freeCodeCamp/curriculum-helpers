2b8bb71a65564a3e4d59a704a5a129e7be5c8d25
Sem Bauke <semboot699@gmail.com>
2022-07-14 17:07:53 +0200

    fix: test not working (#20)
    * fix: test not working
    
    * fix: test
    
    * fix: js tests
    
    * fix: last test

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 import strip from "@freecodecamp/strip-comments";
-import CSSHelp from "./css-helper";
+import { CSSHelp } from "./css-helper"; 
 
 export function initCSSHelper(doc: HTMLDocument): CSSHelp {
   return new CSSHelp(doc);
@@ -21,10 +21,25 @@ export function removeHtmlComments(str: string): string {
  * @returns {String}
  */
 
-
- export function removeCssComments(str: string): string {
+export function removeCssComments(str: string): string {
   return str.replace(/\/\*[\s\S]+?\*\//g, "");
 }
+
+/**
+ * Removes every JS-comment from the string that is provided
+ * @param {String} codeStr a JS-string where the comments need to be removed of
+ * @returns {String}
+ */
+
+export function removeJSComments(codeStr: string): string  {
+  // TODO: publish type declarations and re-enable eslint
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    return strip(codeStr) as string;
+  } catch (err) {
+    return codeStr;
+  }
+};
 
 /**
  * Removes every white-space from the string that is provided
@@ -32,9 +47,8 @@ export function removeHtmlComments(str: string): string {
  * @returns {String}
  */
 
-
- export  function removeWhiteSpace(str: string): string {
-  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+export function removeWhiteSpace(str: string): string {
+  return str.replace(/\s/g, '');
 }
 
 /**
@@ -43,8 +57,7 @@ export function removeHtmlComments(str: string): string {
  * @returns {String}
  */
 
-
- export function escapeRegExp(exp: string): string {
+export function escapeRegExp(exp: string): string {
   return exp.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
 
@@ -55,8 +68,7 @@ export function removeHtmlComments(str: string): string {
  * @returns {Boolean}
  */
 
-
- export  function isCalledWithNoArgs(
+export function isCalledWithNoArgs(
   calledFuncName: string,
   callingCode: string
 ): boolean {
