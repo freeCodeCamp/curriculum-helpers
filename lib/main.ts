554544c0deec7c8040b73ea514978d998f6d7487
Sem Bauke <semboot699@gmail.com>
2022-07-26 13:18:29 +0200

    fix: use right preset hopefully (#40)
    * 1.0.1
    
    * 1.0.2
    
    * 1.0.3
    
    * fix: use right preset hopefully

--- lib/main.ts
+++
@@ -1,6 +0,0 @@
-import * as helper from './index';
-import cssFixtures  from '../lib/__fixtures__/curriculum-helper-css'
-
-const { cssFullExample } = cssFixtures;
-
-console.log(helper.removeCssComments(cssFullExample))
