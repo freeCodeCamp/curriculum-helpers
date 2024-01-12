ecefda973a71627ad164315774bf59a0a0f03070
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-01-12 17:07:35 +0100

    feat: use chainable class for helpers
    This makes for a much cleaner api

--- python/py_helpers.py
+++ python/py_helpers.py
@@ -1,67 +1,54 @@
 import ast
 
-
-def _get_function(mod, func):
-    for node in mod.body:
-        if isinstance(node, ast.FunctionDef):
-            if node.name == func:
-                return node
-    return None
+# A chainable class that allows us to call functions on the result of parsing a string
 
 
-def _local_variable_in_function(func, name):
-    for node in func.body:
-        if isinstance(node, ast.Assign):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    if target.id == name:
-                        return True
-    return False
+class Chainable:
+    def __init__(self, tree=None):
+        self.tree = tree
 
+    def parse(self, string):
+        return Chainable(ast.parse(string))
 
-# This function takes an AST and checks if the function, func, has a local variable with the given name
+    def find_function(self, func):
+        for node in self.tree.body:
+            if isinstance(node, ast.FunctionDef):
+                if node.name == func:
+                    return Chainable(node)
+        return None
 
+    # This function checks the variable, name, is in the current scope
 
-def has_local_variable(mod, func_name, name):
-    func = _get_function(mod, func_name)
-    if func is None:
+    def has_variable(self, name):
+        for node in self.tree.body:
+            if isinstance(node, ast.Assign):
+                for target in node.targets:
+                    if isinstance(target, ast.Name):
+                        if target.id == name:
+                            return True
         return False
 
-    return _local_variable_in_function(func, name)
+    # This function checks the variable, name, is in the current scope and is an integer
 
+    def variable_is_integer(self, name):
+        for node in self.tree.body:
+            if isinstance(node, ast.Assign):
+                for target in node.targets:
+                    if isinstance(target, ast.Name):
+                        if target.id == name:
+                            return type(node.value.value) == type(1)
 
-# This function takes an AST and checks if the variable has the given type
-
-
-def variable_is_integer(mod, name):
-    for node in mod.body:
-        if isinstance(node, ast.Assign):
-            for target in node.targets:
-                if isinstance(target, ast.Name):
-                    if target.id == name:
-                        return type(node.value.value) == type(1)
-
-    return False
-
-
-# This function takes an AST and checks if the local variable has the given type
-
-
-def local_variable_is_integer(mod, func_name, name):
-    func = _get_function(mod, func_name)
-    if func is None:
-        return False
-    return variable_is_integer(func, name)
-
-
-# This function takes two ASTs and checks if the named function is equivalent
-
-
-def functions_are_equivalent(mod_expected, mod_actual, func):
-    func_expected = _get_function(mod_expected, func)
-    func_actual = _get_function(mod_actual, func)
-
-    if func_expected is None or func_actual is None:
         return False
 
-    return ast.dump(func_expected) == ast.dump(func_actual)
+    # This function takes an AST and checks if is equivalent (up to being wrapped in a module) to the chainable's AST
+
+    def is_equivalent(self, target_ast):
+        have_same_dump = ast.dump(self.tree) == ast.dump(target_ast)
+        if have_same_dump:
+            return True
+        else:
+            if isinstance(target_ast, ast.Module):
+                return len(target_ast.body) == 1 and self.is_equivalent(
+                    target_ast.body[0]
+                )
+            return False
--- python/py_helpers.test.py
+++ python/py_helpers.test.py
@@ -1,29 +1,127 @@
 import unittest
 import ast
-import py_helpers
+from py_helpers import Chainable
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_has_local_variable(self):
-        two_locals = """
-def foo():
+    def test_parse_creates_chainable(self):
+        chainable = Chainable().parse("def foo():\n  pass")
+
+        self.assertTrue(isinstance(chainable.tree, ast.Module))
+        self.assertEqual(
+            ast.dump(chainable.tree), ast.dump(ast.parse("def foo():\n  pass"))
+        )
+
+    def test_find_function_returns_function_ast(self):
+        chainable = Chainable().parse("def foo():\n  pass")
+
+        func = chainable.find_function("foo")
+
+        self.assertTrue(isinstance(func.tree, ast.FunctionDef))
+        self.assertEqual(func.tree.name, "foo")
+
+    def test_find_function_returns_none(self):
+        chainable = Chainable().parse("def foo():\n  pass")
+
+        func = chainable.find_function("bar")
+
+        self.assertEqual(func, None)
+
+    def test_has_local_variable_in_function(self):
+        func_str = """def foo():
   a = 1
+  print(a)
   x = 2
 """
-        mod = ast.parse(two_locals)
-        self.assertTrue(py_helpers.has_local_variable(mod, "foo", "x"))
-        self.assertFalse(py_helpers.has_local_variable(mod, "foo", "y"))
-        self.assertFalse(py_helpers.has_local_variable(mod, "bar", "x"))
-        self.assertFalse(py_helpers.has_local_variable(mod, "bar", "y"))
 
-    def test_variable_is_integer(self):
-        two_vars = """
-x = "1"
-y = 2
+        chainable = Chainable().parse(func_str)
+
+        self.assertTrue(chainable.find_function("foo").has_variable("x"))
+
+    def test_has_global_variable(self):
+        globals_str = """a = 1
+x = 2
 """
-        mod = ast.parse(two_vars)
-        self.assertFalse(py_helpers.variable_is_integer(mod, "x"))
-        self.assertTrue(py_helpers.variable_is_integer(mod, "y"))
+
+        chainable = Chainable().parse(globals_str)
+
+        self.assertTrue(chainable.has_variable("x"))
+
+    def test_does_not_see_local_variables_out_of_scope(self):
+        scopes_str = """def foo():
+  a = 1
+b = 2
+"""
+
+        chainable = Chainable().parse(scopes_str)
+
+        self.assertFalse(chainable.has_variable("a"))
+
+    def test_nested_function(self):
+        nested_str = """def foo():
+  def bar():
+    x = 1
+  y = 2
+"""
+
+        chainable = Chainable().parse(nested_str)
+
+        self.assertTrue(chainable.find_function("foo").has_variable("y"))
+        self.assertFalse(chainable.find_function("foo").has_variable("x"))
+        self.assertTrue(
+            chainable.find_function("foo").find_function("bar").has_variable("x")
+        )
+
+    def test_is_equivalent(self):
+        full_str = """def foo():
+  a = 1
+  print(a)
+def bar():
+  x = "1"
+  print(x)
+"""
+
+        chainable = Chainable().parse(full_str)
+
+        expected = ast.parse(
+            """def bar():
+  x = "1"
+  print(x)
+"""
+        )
+
+        self.assertTrue(chainable.find_function("bar").is_equivalent(expected))
+        # Obviously, it should be equivalent to itself
+        self.assertTrue(
+            chainable.find_function("bar").is_equivalent(
+                chainable.find_function("bar").tree
+            )
+        )
+        # It should also be equivalent to the FunctionDef node itself
+        self.assertTrue(chainable.find_function("bar").is_equivalent(expected.body[0]))
+
+    def test_is_not_equivalent(self):
+        full_str = """def foo():
+  a = 1
+  print(a)
+def bar():
+  x = "1"
+  print(x)
+"""
+        chainable = Chainable().parse(full_str)
+        # this should not be equivalent because it contains an extra function
+
+        expected = ast.parse(
+            """def bar():
+  x = "1"
+  print(x)
+
+def foo():
+  a = 1
+"""
+        )
+
+        self.assertFalse(chainable.find_function("bar").is_equivalent(expected))
 
     def test_local_variable_is_integer(self):
         two_locals = """
@@ -32,51 +130,15 @@ def foo():
   print(a)
   x = 2
 """
-        mod = ast.parse(two_locals)
-        self.assertTrue(py_helpers.local_variable_is_integer(mod, "foo", "x"))
-        self.assertFalse(py_helpers.local_variable_is_integer(mod, "foo", "y"))
+        chainable = Chainable().parse(two_locals)
 
-        modWithString = ast.parse('def foo():\n  x = "1"')
-        self.assertFalse(
-            py_helpers.local_variable_is_integer(modWithString, "foo", "x")
-        )
+        self.assertTrue(chainable.find_function("foo").variable_is_integer("x"))
+        self.assertFalse(chainable.find_function("foo").variable_is_integer("y"))
 
-    def test_functions_are_equivalent(self):
-        func_str_actual = """def a(
-  b,
-    c,
-):
-    print(b)
-a = 'variable'"""
-        func_str_expected = """another = 'variable'
-def a(b, c):
-    print(b)"""
+    def test_local_variable_is_integer_with_string(self):
+        chainable = Chainable().parse('def foo():\n  x = "1"')
 
-        mod_expected = ast.parse(func_str_expected)
-        mod_actual = ast.parse(func_str_actual)
-
-        self.assertTrue(
-            py_helpers.functions_are_equivalent(mod_expected, mod_actual, "a")
-        )
-
-    def test_functions_are_not_equivalent(self):
-        func_str_actual = """def a(
-  b,
-    d,
-):
-    print(b)
-a = 'variable'"""
-        # different arg name
-        func_str_expected = """another = 'variable'
-def a(b, c):
-    print(b)"""
-
-        mod_expected = ast.parse(func_str_expected)
-        mod_actual = ast.parse(func_str_actual)
-
-        self.assertFalse(
-            py_helpers.functions_are_equivalent(mod_expected, mod_actual, "a")
-        )
+        self.assertFalse(chainable.find_function("foo").variable_is_integer("x"))
 
 
 if __name__ == "__main__":
