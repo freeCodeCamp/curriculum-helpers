e58e9bf662fb2f35184ce572d32ff9675760d8e0
Shaun Hamilton <shauhami020@gmail.com>
2024-01-17 12:00:57 +0200

    chore: use webpack to build (#110)
    * chore: use webpack to build
    
    * remove recommendations made by webpack
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
    
    ---------
    
    Co-authored-by: Oliver Eyton-Williams <ojeytonwilliams@gmail.com>

--- lib/strip.ts
+++ lib/strip.ts
@@ -6,8 +6,8 @@
  * Copyright (c) 2014-present, Jon Schlinkert.
  * Released under the MIT License.
  */
-import { compile } from "./compile.js";
-import { parse } from "./parse.js";
+import { compile } from "./compile";
+import { parse } from "./parse";
 
 /**
  * Strip all code comments from the given `input`, including protected
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+console.info("Building in " + process.env.NODE_ENV + " mode.");
+module.exports = {
+  mode: process.env.NODE_ENV ? "production" : "development",
+  entry: {
+    index: "./lib/index.ts",
+  },
+  devtool: process.env.NODE_ENV ? "source-map" : "inline-source-map",
+  output: {
+    library: {
+      name: "helpers",
+      type: "umd",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, "lib"),
+        loader: "ts-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+    extensionAlias: {
+      ".ts": [".js", ".ts"],
+    },
+  },
+};
