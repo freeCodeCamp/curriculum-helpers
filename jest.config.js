064d874e234428ed898957b0a324272d78e00442
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-01-29 13:06:51 +0100

    fix: remove mocks from published files (#128)
    * fix: remove mocks from published files
    
    * chore: exclude __mocks__ from tsc

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const config = {
   moduleNameMapper: {
     // We don't use the python scripts in the tests, so we can mock them with
     // a simple script that does nothing.
-    "python/(.*.py)": "<rootDir>/python/mock-script.ts",
+    "python/(.*.py)": "<rootDir>/python/__mocks__/mock-script.ts",
   },
 };
 
