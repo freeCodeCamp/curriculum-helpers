3b7a65ee5335790ab9e9e3754d100cbfafb8b90a
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2024-02-01 15:54:00 +0100

    refactor: group functions in describe blocks

--- lib/__tests__/python.test.ts
+++ lib/__tests__/python.test.ts
@@ -2,8 +2,9 @@ import * as helper from "../index";
 
 describe("python", () => {
   const { python } = helper;
-  it("getDef", () => {
-    const code = `
+  describe("getDef", () => {
+    it("finds function, body, indentation and params", () => {
+      const code = `
 a = 1
 
 def b(d, e):
@@ -12,21 +13,27 @@ def b(d, e):
 def c():
   a = 1
 `;
-    const match = python.getDef(code, "b");
-    if (match) {
-      const { def, function_indentation, function_body, function_parameters } =
-        match;
-      expect(def).toEqual(`def b(d, e):
+      const match = python.getDef(code, "b");
+      if (match) {
+        const {
+          def,
+          function_indentation,
+          function_body,
+          function_parameters,
+        } = match;
+        expect(def).toEqual(`def b(d, e):
   a = 2
 `);
-      expect(function_indentation).toEqual(0);
-      expect(function_body).toEqual("  a = 2\n");
-      expect(function_parameters).toEqual("d, e");
-    }
+        expect(function_indentation).toEqual(0);
+        expect(function_body).toEqual("  a = 2\n");
+        expect(function_parameters).toEqual("d, e");
+      }
+    });
   });
 
-  it("removeComments", () => {
-    const code = `
+  describe("removeComments", () => {
+    it("removes comments without removing whitespace", () => {
+      const code = `
 a = 1
 # comment
 def b(d, e):
@@ -34,8 +41,8 @@ def b(d, e):
   # comment
   return a #comment
 `;
-    const result = python.removeComments(code);
-    expect(result).toEqual(`
+      const result = python.removeComments(code);
+      expect(result).toEqual(`
 a = 1
 
 def b(d, e):
@@ -43,10 +50,12 @@ def b(d, e):
   
   return a 
 `);
+    });
   });
 
-  it("getBlock", () => {
-    const code = `
+  describe("getBlock", () => {
+    it("finds the body of an if statement", () => {
+      const code = `
 a = 1
 
 if a == 1:
@@ -58,45 +67,46 @@ if a == 1:
 for i in range(10):
   a = 1
 `;
-    const matches = [
-      python.getBlock(code, "if a == 1"),
-      python.getBlock(code, /if +\w+ *== *\d+/),
-      // eslint-disable-next-line prefer-regex-literals
-      python.getBlock(code, new RegExp("if +\\w+ *== *\\d+")),
-    ];
-    for (const match of matches) {
-      expect(match).not.toBeNull();
-      if (match) {
-        // eslint-disable-next-line camelcase
-        const { block_indentation, block_body, block_condition } = match;
-        expect(block_condition).toEqual("if a == 1");
-        expect(block_indentation).toEqual(0);
-        expect(block_body).toEqual(
-          `  a = 2
+      const matches = [
+        python.getBlock(code, "if a == 1"),
+        python.getBlock(code, /if +\w+ *== *\d+/),
+        // eslint-disable-next-line prefer-regex-literals
+        python.getBlock(code, new RegExp("if +\\w+ *== *\\d+")),
+      ];
+      for (const match of matches) {
+        expect(match).not.toBeNull();
+        if (match) {
+          // eslint-disable-next-line camelcase
+          const { block_indentation, block_body, block_condition } = match;
+          expect(block_condition).toEqual("if a == 1");
+          expect(block_indentation).toEqual(0);
+          expect(block_body).toEqual(
+            `  a = 2
   b = 3
   if b == 3:
     a = 4
 `
-        );
+          );
+        }
       }
-    }
 
-    const matches2 = [
-      python.getBlock(code, "for i in range(10)"),
-      python.getBlock(code, /for +\w+ +in +range\(\d+\)/),
-      // eslint-disable-next-line prefer-regex-literals
-      python.getBlock(code, new RegExp("for +\\w+ +in +range\\(\\d+\\)")),
-    ];
-    for (const match of matches2) {
-      expect(match).not.toBeNull();
-      if (match) {
-        // eslint-disable-next-line camelcase
-        const { block_indentation, block_body, block_condition } = match;
-        expect(block_condition).toEqual("for i in range(10)");
-        expect(block_indentation).toEqual(0);
-        expect(block_body).toEqual(`  a = 1
+      const matches2 = [
+        python.getBlock(code, "for i in range(10)"),
+        python.getBlock(code, /for +\w+ +in +range\(\d+\)/),
+        // eslint-disable-next-line prefer-regex-literals
+        python.getBlock(code, new RegExp("for +\\w+ +in +range\\(\\d+\\)")),
+      ];
+      for (const match of matches2) {
+        expect(match).not.toBeNull();
+        if (match) {
+          // eslint-disable-next-line camelcase
+          const { block_indentation, block_body, block_condition } = match;
+          expect(block_condition).toEqual("for i in range(10)");
+          expect(block_indentation).toEqual(0);
+          expect(block_body).toEqual(`  a = 1
 `);
+        }
       }
-    }
+    });
   });
 });
