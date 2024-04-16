d0db232ab45d96231143be68074ec967d24e7ac5
Anna <a.rcottrill521@gmail.com>
2024-04-16 12:34:05 -0400

    feat: create a JS helpers class  (#190)
    * feat: create js helpers
    
    * capture arrow functions
    
    * make everything more readable

---
+++ lib/__tests__/javascript-helper.test.ts
@@ -0,0 +1,36 @@
+import jsTestValues from "../__fixtures__/curriculum-helpers-javascript";
+
+import { getFunctionParams } from "../index";
+
+const { functionDeclaration, constFunction, letFunction, arrowFunction } =
+  jsTestValues;
+
+describe("js-help", () => {
+  describe("getFunctionArgs", () => {
+    it("gets arguments from function declarations", function () {
+      const parameters = getFunctionParams(functionDeclaration);
+      expect(parameters[0].name).toBe("param1");
+      expect(parameters[1].defaultValue).toBe("default");
+      expect(parameters[1].name).toBe("param2");
+      expect(parameters[2].name).toBe("param3");
+    });
+    it("gets arguments from const function variables", function () {
+      const parameters = getFunctionParams(constFunction);
+      expect(parameters[0].name).toBe("param1");
+      expect(parameters[1].defaultValue).toBe("default");
+      expect(parameters[1].name).toBe("param2");
+      expect(parameters[2].name).toBe("param3");
+    });
+    it("gets arguments from let function variables", function () {
+      const parameters = getFunctionParams(letFunction);
+      expect(parameters[0].name).toBe("param1");
+      expect(parameters[1].defaultValue).toBe("default");
+      expect(parameters[1].name).toBe("param2");
+      expect(parameters[2].name).toBe("param3");
+    });
+    it("gets arguments from arrow functions", function () {
+      const parameters = getFunctionParams(arrowFunction);
+      expect(parameters[0].name).toBe("name");
+    });
+  });
+});
