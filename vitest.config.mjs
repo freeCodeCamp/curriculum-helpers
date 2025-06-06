4b540e5b65db7ee83742c25b513183981c3aec6b
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-06-06 11:44:14 +0200

    test: only watch .ts files, trigger related tests
    All unit tests follow the pattern filename.ts -> filename.test.ts, so
    vitest now always looks for the related test.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,5 +1,10 @@
 import { configDefaults, defineConfig } from "vitest/config";
 
+const integrationTests = [
+  "./packages/main/integration-tests/index.test.ts",
+  "./packages/main/integration-tests/timeout.test.ts",
+];
+
 export default defineConfig({
   assetsInclude: ["**/*.py"],
   test: {
@@ -9,13 +14,17 @@ export default defineConfig({
     globalSetup: "vitest-environment-puppeteer/global-init",
     watchTriggerPatterns: [
       {
-        pattern: /packages/,
-        testsToRun: (id, match) => {
-          // In principle any source change could impact the integration tests,
-          return [
-            `./packages/main/integration-tests/index.test.ts`,
-            `./packages/main/integration-tests/timeout.test.ts`,
-          ];
+        pattern: /packages.*\.ts$/,
+        testsToRun: (id) => {
+          // If the changed file is a test, it's the only test that could be
+          // affected.
+          const isTestFile = id.endsWith(".test.ts");
+          if (isTestFile) return id;
+
+          // Otherwise, if there is a test file, this is it:
+          const testFile = id.slice(0, -3) + ".test.ts";
+          // In principle any source change could impact the integration tests
+          return [...integrationTests, testFile];
         },
       },
     ],
