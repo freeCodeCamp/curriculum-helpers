64daba45ee0803d61b1da5f79d49b15af9cee035
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-06-06 10:32:03 +0200

    refactor: rename integration tests and improve watch
    Now they are triggered by any source change.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -7,5 +7,17 @@ export default defineConfig({
     exclude: [...configDefaults.exclude, "packages/*/build", "dist"],
     globals: true, // TODO: remove this OR include vitest types
     globalSetup: "vitest-environment-puppeteer/global-init",
+    watchTriggerPatterns: [
+      {
+        pattern: /packages/,
+        testsToRun: (id, match) => {
+          // In principle any source change could impact the integration tests,
+          return [
+            `./packages/main/integration-tests/index.test.ts`,
+            `./packages/main/integration-tests/timeout.test.ts`,
+          ];
+        },
+      },
+    ],
   },
 });
