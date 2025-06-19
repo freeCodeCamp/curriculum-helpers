80ab3771fb42824f7b27514676ae1bfcb8b32c38
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-06-19 15:19:29 +0200

    refactor: put afterEach at end of runTest
    It's simpler this way.

--- packages/dom-evaluator/src/dom-test-evaluator.ts
+++ packages/dom-evaluator/src/dom-test-evaluator.ts
@@ -129,20 +129,10 @@ ${testString}`);
           await test();
         }
 
-        if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
-
         return { pass: true, ...this.#flushLogs() };
       } catch (err) {
         this.#proxyConsole.off();
         console.error(err);
-
-        try {
-          if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
-        } catch (afterEachErr) {
-          // Even though we're returning the original test error, we still
-          // want to log for debugging purposes.
-          console.error("Error in afterEach hook:", afterEachErr);
-        }
 
         const error = err as Fail["err"];
         // To provide useful debugging information when debugging the tests, we
@@ -159,6 +149,14 @@ ${testString}`);
         };
       } finally {
         this.#proxyConsole.off();
+
+        try {
+          if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
+        } catch (afterEachErr) {
+          // Even though we're returning the original test error, we still
+          // want to log for debugging purposes.
+          console.error("Error in afterEach hook:", afterEachErr);
+        }
       }
     };
 
--- packages/javascript-evaluator/src/javascript-test-evaluator.ts
+++ packages/javascript-evaluator/src/javascript-test-evaluator.ts
@@ -78,20 +78,10 @@ ${test};`);
           }
         }
 
-        if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
-
         return { pass: true, ...this.#flushLogs() };
       } catch (err: unknown) {
         this.#proxyConsole.off();
         console.error(err);
-
-        try {
-          if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
-        } catch (afterEachErr) {
-          // Even though we're returning the original test error, we still
-          // want to log for debugging purposes.
-          console.error("Error in afterEach hook:", afterEachErr);
-        }
 
         const error = err as Fail["err"];
 
@@ -106,6 +96,14 @@ ${test};`);
         };
       } finally {
         this.#proxyConsole.off();
+
+        try {
+          if (opts.hooks?.afterEach) eval(opts.hooks.afterEach);
+        } catch (afterEachErr) {
+          // Even though we're returning the original test error, we still
+          // want to log for debugging purposes.
+          console.error("Error in afterEach hook:", afterEachErr);
+        }
       }
     };
   }
