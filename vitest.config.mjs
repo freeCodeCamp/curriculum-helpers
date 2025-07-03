7a93d48cd7158c216191a409ceec16437087f719
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-07-03 17:57:00 +0200

    fix: use concurrently to give correct watch behaviour
    Vitest has projects which are a great way to organise test config, but
    watchTriggerPatterns is global, not per project. If you want a single
    project to behave like e2e tests (i.e. run if the source changes)
    and another like unit tests (i.e. run if a file they're importing
    changes), it doesn't work.
    
    Concurrently fixes this because it handles running the projects and they
    handle (separately) which tests should run.

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,32 +1,7 @@
-import { configDefaults, defineConfig } from "vitest/config";
-
-const integrationTests = [
-  "./packages/main/integration-tests/index.test.ts",
-  "./packages/main/integration-tests/timeout.test.ts",
-];
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  assetsInclude: ["**/*.py"],
   test: {
-    environment: "puppeteer",
-    exclude: [...configDefaults.exclude, "packages/*/build", "dist"],
-    globals: true, // TODO: remove this OR include vitest types
-    globalSetup: "vitest-environment-puppeteer/global-init",
-    watchTriggerPatterns: [
-      {
-        pattern: /packages.*\.ts$/,
-        testsToRun: (id) => {
-          // If the changed file is a test, it's the only test that could be
-          // affected.
-          const isTestFile = id.endsWith(".test.ts");
-          if (isTestFile) return id;
-
-          // Otherwise, if there is a test file, this is it:
-          const testFile = id.slice(0, -3) + ".test.ts";
-          // In principle any source change could impact the integration tests
-          return [...integrationTests, testFile];
-        },
-      },
-    ],
+    projects: ["./vitest.integration.config.mjs", "./vitest.unit.config.mjs"],
   },
 });
