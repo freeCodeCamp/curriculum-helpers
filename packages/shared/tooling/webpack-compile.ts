2fd9b2bb4168602d6170d438f30130afa8d4c8f5
Oliver Eyton-Williams <ojeytonwilliams@gmail.com>
2025-07-04 13:04:47 +0200

    chore: warn if webpack fails to create the fixtures
    It's quite possible for webpack's cache to get messed up and then it can
    silently fail to build.

--- packages/shared/tooling/webpack-compile.ts
+++ packages/shared/tooling/webpack-compile.ts
@@ -1,6 +1,17 @@
 import { spawnSync } from "child_process";
+import { rmSync, existsSync } from "fs";
+import path from "path";
+
+const __dirname = new URL(".", import.meta.url).pathname;
+
+const FIXTURES_DIR = path.join(__dirname, "../../../__fixtures__/dist");
 
 export const compileForTests = () => {
+  // It's necessary to manually remove the previous build artifacts so we can
+  // check that the build is successful. This should not be necessary, but
+  // webpack can fail silently.
+  rmSync(FIXTURES_DIR, { recursive: true, force: true });
+
   // It's necessary spawnSync the webpack process to ensure that the build is
   // complete before the tests start
   const result = spawnSync(
@@ -11,6 +22,14 @@ export const compileForTests = () => {
       encoding: "utf8",
     },
   );
+
+  if (!existsSync(FIXTURES_DIR)) {
+    console.log(result.stdout);
+    console.error(result.stderr);
+    console.error(
+      `Webpack build failed. The dist folder, ${FIXTURES_DIR}, was not created.`,
+    );
+  }
 
   // Only log if there is an error
   if (result.status) {
